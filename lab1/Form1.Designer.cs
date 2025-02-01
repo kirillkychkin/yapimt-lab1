76ff6a400d8bdb3cf53d51a337443654bdf2e0dd
Kirill Kychkin <kychkin.kirill@gmail.com>
2025-02-01 21:17:12 +0900

    add menu, add count multiples of seven

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            выходToolStripMenuItem = new ToolStripMenuItem();
+            выйтиИзПрограммыToolStripMenuItem = new ToolStripMenuItem();
+            справкаToolStripMenuItem = new ToolStripMenuItem();
+            оПрограммеToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            button1 = new Button();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { выходToolStripMenuItem, справкаToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            выходToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { выйтиИзПрограммыToolStripMenuItem });
+            выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            выходToolStripMenuItem.Size = new Size(53, 20);
+            выходToolStripMenuItem.Text = "Выход";
+            // 
+            // выйтиИзПрограммыToolStripMenuItem
+            // 
+            выйтиИзПрограммыToolStripMenuItem.Name = "выйтиИзПрограммыToolStripMenuItem";
+            выйтиИзПрограммыToolStripMenuItem.Size = new Size(193, 22);
+            выйтиИзПрограммыToolStripMenuItem.Text = "Выйти из программы";
+            выйтиИзПрограммыToolStripMenuItem.Click += выйтиИзПрограммыToolStripMenuItem_Click;
+            // 
+            // справкаToolStripMenuItem
+            // 
+            справкаToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { оПрограммеToolStripMenuItem });
+            справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            справкаToolStripMenuItem.Size = new Size(65, 20);
+            справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            оПрограммеToolStripMenuItem.Size = new Size(149, 22);
+            оПрограммеToolStripMenuItem.Text = "О программе";
+            оПрограммеToolStripMenuItem.Click += оПрограммеToolStripMenuItem_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(310, 15);
+            label1.TabIndex = 1;
+            label1.Text = "1. Найти сумму чисел кратных семи от нуля до тысячи";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(369, 39);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Вычислить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem выходToolStripMenuItem;
+        private ToolStripMenuItem выйтиИзПрограммыToolStripMenuItem;
+        private ToolStripMenuItem справкаToolStripMenuItem;
+        private ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private Label label1;
+        private Button button1;
     }
 }
--- lab1/Form1.cs
+++ lab1/Form1.cs
@@ -6,5 +6,36 @@ namespace lab1
         {
             InitializeComponent();
         }
+
+        private string getAboutText()
+        {
+            return "Программа разработана в 2025 году в рамках каурса 'Языки программирования и методы трансляции' в качестве первой лабораторной работы." + Environment.NewLine + Environment.NewLine + "Программа умеет: " + Environment.NewLine + "1. Находить сумму чисел кратных семи от нуля до тысячи" + Environment.NewLine + "2. По заданным координатам треугольника и точки вычислить, находится ли точка внутри треугольника" + Environment.NewLine + "3. Не понял что должна делать";
+        }
+
+        private int countMultiplesOfSeven()
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i+=7)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        private void выйтиИзПрограммыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(getAboutText());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Сумма чисел кратных семи от нуля до тысячи: " + countMultiplesOfSeven());
+        }
     }
 }
